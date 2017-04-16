ab3e14025959a6bf1c2e05cb33d630aa81a176d5
BranLiang <lby89757@hotmail.com>
2017-04-16 11:17:35 +0800

    add configs

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope :api do
+    resources :events
+
+    resources :users, except: :put
+
+    resources :password_resets, only: [:show, :create, :update],
+                                param: :reset_token
+    resources :user_confirmations, only: :show, param: :confirmation_token
+    resources :access_tokens, only: :create do
+      delete '/', action: :destroy, on: :collection
+    end
+  end
 end
