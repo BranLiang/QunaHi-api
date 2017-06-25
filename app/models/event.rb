7580ea71cabd62dff637f6a1934abaa2195687e1
Bran Liang <lby89757@hotmail.com>
2017-06-25 14:57:12 +0800

    add image model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ApplicationRecord
-  validates :name, :description, presence: true
+  has_many :images, as: :imageable
+
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :description, presence: true
 end
